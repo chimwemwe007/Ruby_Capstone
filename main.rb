32d18ba6046370ec74bed1b97577f5be542580f3
bengigenc <genc.bengi@gmail.com>
2022-10-17 21:27:06 +0300

    Add main.rb file with a list of options for the user

---
+++ main.rb
@@ -0,0 +1,15 @@
+# prepare options here
+@options = [
+  '1: List all books',
+  '2: List all labels',
+  '3: Add a book',
+  '4: List all music albums',
+  '5: List all genres',
+  '6: Add a music album',
+  '7: List all games',
+  '8: List all authors',
+  '9: Add a game',
+  '10: Exit'
+]
+
+
