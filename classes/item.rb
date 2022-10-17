4edbc66dfa3ce07f39aa7a99f5a9a5d50f8219ff
bengigenc <genc.bengi@gmail.com>
2022-10-17 21:40:19 +0300

    Fix linter issues

--- classes/item.rb
+++ classes/item.rb
@@ -1,7 +1,7 @@
 require 'date'
 
 class Item
-  def initialize(id, genre, author, source, label, publish_date, archived: true)
+  def initialize(_id, genre, author, source, label, publish_date, archived: true)
     @id = Random.rand(1..1000)
     @genre = genre
     @author = author
@@ -12,17 +12,14 @@ class Item
   end
 
   private
+
   def can_be_archived?
- ( Time.new.year - @publish_date ) > 10
+    (Time.new.year - @publish_date) > 10
   end
 
-
-  private
   def move_to_archive
     @archived = true if can_be_archived?
   end
 
-  
   attr_reader :id, :archived
 end
-
