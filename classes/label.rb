f465439e3ce32510170f1d3fb1c0a31b5975887a
Kidist Dinku <95876178+Ney2@users.noreply.github.com>
2022-10-18 13:08:38 +0300

    Update label class

--- classes/label.rb
+++ classes/label.rb
@@ -1,12 +1,8 @@
 class Label
-
-    attr_reader: id, :title, :color
-    attr_accessor: item 
-
-    def initialize(id, title, color, item)
-        @id = Random.rand(1..1000)
-        @title = title
-        @color = color
-        @item = item
-    end
+  def initialize(_id, title, color)
+    @id = Random.rand(1..1000)
+    @title = title
+    @color = color
+    @items = []
+  end
 end
