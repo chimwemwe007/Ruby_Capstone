79a5c1b3aa7954c32f031eb07b8a156c027ceafc
Kidist Dinku <95876178+Ney2@users.noreply.github.com>
2022-10-18 18:38:23 +0300

    Update item class

--- classes/item.rb
+++ classes/item.rb
@@ -1,14 +1,30 @@
 require 'date'
 
 class Item
-  def initialize(_id, genre, author, source, label, publish_date, archived: true)
-    @id = Random.rand(1..1000)
-    @genre = genre
-    @author = author
-    @source = source
-    @label = label
+  attr_reader :id, :genre, :author, :label, :publish_date
+
+  def initialize(id, publish_date)
+    @id = id || SecureRandom.uuid
     @publish_date = publish_date
-    @archived = archived
+  end
+
+  def move_to_archive
+    @archived = true if can_be_archived?
+  end
+
+  def add_author(author)
+    @author = author
+    author.items.push(self) unless author.items.include?(self)
+  end
+
+  def add_genre(genre)
+    @genre = genre
+    genre.items.push(self) unless genre.items.include?(self)
+  end
+
+  def add_label(label)
+    @label = label
+    label.items.push(self) unless label.items.include?(self)
   end
 
   private
@@ -16,10 +32,4 @@ class Item
   def can_be_archived?
     (Time.new.year - @publish_date) > 10
   end
-
-  def move_to_archive
-    @archived = true if can_be_archived?
-  end
-
-  attr_reader :id, :archived
 end
