09bbe6d705933d0dfe0528ed39775a8da1c1ee52
bengigenc <genc.bengi@gmail.com>
2022-10-19 12:48:03 +0300

    Update argument

--- classes/author.rb
+++ classes/author.rb
@@ -1,8 +1,8 @@
 class Author
   attr_accessor :items, :id, :first_name, :last_name
 
-  def initialize(first_name, last_name)
-    @id = Random.rand(1..1000)
+  def initialize(id, first_name, last_name)
+    # @id = Random.rand(1..1000)
     @first_name = first_name
     @last_name = last_name
     @items = []
