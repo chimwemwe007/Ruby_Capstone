09161fdbef0e25bf6e34b3389d193862b6ff41b4
bengigenc <genc.bengi@gmail.com>
2022-10-19 18:51:26 +0300

    Get value inputs to create game

--- menu_options/add_game.rb
+++ menu_options/add_game.rb
@@ -10,7 +10,22 @@ def add_game
   last_name = gets.chomp
 
   # get the values for game
+  puts 'Publish date of the game:'
+  publish_date = gets.chomp
+
+  puts 'Is it a multiplayer game? [Y/N]'
+  multiplayer = gets.chomp.upcase
+  case multiplayer
+    when 'Y'
+      multiplayer = true
+    when 'N'
+      multiplayer = false
+    end
   
+  puts 'What is the last played date? [YYYY-MM-DD]'
+  date_input = gets.chomp
+  last_played_at = DateTime.parse(date_input).to_date
+
 
   # create the game
   
