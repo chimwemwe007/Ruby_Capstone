5ac272146bdc4cfa878b954bf08dcdefef1dfa08
bengigenc <genc.bengi@gmail.com>
2022-10-20 22:29:11 +0300

    Add item table to schema

--- schema.sql
+++ schema.sql
@@ -1,3 +1,17 @@
+CREATE TABLE item (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    publish_date DATE NOT NULL,
+    archived BOOLEAN NOT NULL DEFAULT FALSE,
+    author_id INTEGER NOT NULL,
+    source_id INTEGER NOT NULL,
+    label_id INTEGER NOT NULL,
+    genre_id INTEGER NOT NULL,
+    FOREIGN KEY (author_id) REFERENCES author (id),
+    FOREIGN KEY (source_id) REFERENCES source (id),
+    FOREIGN KEY (label_id) REFERENCES label (id),
+    FOREIGN KEY (genre_id) REFERENCES genre (id)
+);
+
 CREATE TABLE book
 (
   id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
