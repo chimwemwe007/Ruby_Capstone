0702fcaf3cc4d5a39ee99c2be0344b7f17dec0be
Kidist Dinku <95876178+Ney2@users.noreply.github.com>
2022-10-20 23:28:42 +0300

    Save data in json format

--- data/preserve_book_data.rb
+++ data/preserve_book_data.rb
@@ -1,57 +1,89 @@
 require 'json'
 require_relative '../classes/book'
 require_relative '../classes/label'
+require_relative '../app'
 
-class PreserveBookData
-  attr_accessor :books, :labels
+def load_books
+  if File.exist?('./data/books.json')
+    file = File.open('./data/books.json')
 
-  def initialize
-    @books = []
-    @labels = []
-  end
-
-  def read_books
-    return unless File.exist?('books.json')
-
-    books_loaded = JSON.parse(File.read('books.json'))
-    books_loaded.each do |book|
-      new_book = Book.new(book['id'], book['publish_date'], book['publisher'], book['cover_state'])
-      new_label = @labels.select { |label| label.id == book['label_id'] }[0]
-      new_book.add_label(new_label)
-      @books << new_book
-    end
-  end
-
-  def read_labels
-    return unless File.exist?('labels.json')
-
-    labels_loaded = JSON.parse(File.read('labels.json'))
-    labels_loaded.each do |label|
-      new_label = Label.new(label['id'], label['title'], label['color'])
-      @labels << new_label
-    end
-  end
-
-  def save_book(book)
-    new_book = { id: book.id, publish_date: book.publish_date, publisher: book.publisher,
-                 cover_state: book.cover_state, label_id: book.label.id }
-    if File.exist?('books.json')
-      books_loaded = JSON.parse(File.read('books.json'))
-      books_loaded << new_book
-      File.write('books.json', JSON.pretty_generate(books_loaded))
+    if file.size.zero?
+      'There is nothing saved yet.'
     else
-      File.write('books.json', JSON.pretty_generate([new_book]))
-    end
-  end
+      books = JSON.parse(File.read('./data/books.json'))
 
-  def save_label(label)
-    new_label = { id: label.id, title: label.title, color: label.color }
-    if File.exist?('labels.json')
-      labels_loaded = JSON.parse(File.read('labels.json'))
-      labels_loaded << new_label
-      File.write('labels.json', JSON.pretty_generate(labels_loaded))
+      books.each do |book|
+        book = Book.new(book['publish_date'], book['publisher'], book['cover_state'])
+        @books << book
+      end
+    end
+    file.close
+  end
+end
+
+def load_labels
+  if File.exist?('./data/labels.json')
+    file = File.open('./data/labels.json')
+
+    if file.size.zero?
+      'There is nothing saved yet.'
     else
-      File.write('labels.json', JSON.pretty_generate([new_label]))
+      labels = JSON.parse(File.read('./data/labels.json'))
+
+      labels.each do |label|
+        label = Label.new(label['title'], label['color'])
+        @labels << label
+      end
+    end
+    file.close
+  end
+end
+
+def save_book(publish_date, publisher, cover_state)
+  obj = {
+    publish_date: publish_date,
+    publisher: publisher,
+    cover_state: cover_state
+  }
+
+  if File.exist?('./data/books.json')
+    file = File.open('./data/books.json')
+
+    if file.size.zero?
+      book = [obj]
+    else
+      book = JSON.parse(File.read('./data/books.json'))
+      book << obj
+    end
+
+    file.close
+
+    File.open('./data/books.json', 'w') do |f|
+      f.write(JSON.pretty_generate(book))
     end
   end
 end
+
+  def save_label(title, color)
+    obj = {
+      title: title,
+      color: color
+    }
+
+    if File.exist?('./data/labels.json')
+      file = File.open('./data/labels.json')
+
+      if file.size.zero?
+        label = [obj]
+      else
+        label = JSON.parse(File.read('./data/labels.json'))
+        label << obj
+      end
+
+      file.close
+
+      File.open('./data/labels.json', 'w') do |f|
+        f.write(JSON.pretty_generate(label))
+      end
+    end
+  end
