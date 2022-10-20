3ae7b8fb27fe652f9ecbca2093851c71cac84065
Kidist Dinku <95876178+Ney2@users.noreply.github.com>
2022-10-21 00:34:10 +0300

    Resolve confilict

--- classes/book.rb
+++ classes/book.rb
@@ -3,7 +3,7 @@ require_relative './item'
 class Book < Item
   attr_reader :publisher, :cover_state
 
-  def initialize(_id, publish_date, publisher, cover_state)
+  def initialize(publish_date, publisher, cover_state)
     super(publish_date)
     @publisher = publisher
     @cover_state = cover_state
